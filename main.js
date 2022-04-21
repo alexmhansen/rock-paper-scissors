41c6c8adbc4387fbe8b3bf06ffc1c5710dad34a6
Alexander Hansen <alexmhansen13@gmail.com>
2022-04-21 02:34:54 -0700

    Added index.html and main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Return one of three random values
+function computerPlay() {
+    let random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return 'rock';
+    } else if (random === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    } 
+}
